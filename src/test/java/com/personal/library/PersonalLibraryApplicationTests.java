eaf9e380f22754a500d0d940b671dcc0eeca52b1
tuche24 <tuche24@naver.com>
2021-05-30 22:20:16 +0900

    personalLibrary

---
+++ src/test/java/com/personal/library/PersonalLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
