27872a8bbac3bd99932883d0b8f392e628207a06
tuche24 <tuche24@naver.com>
2021-06-01 22:24:47 +0900

    postCode

--- src/main/java/com/personal/controller/locationController.java
+++ src/main/java/com/personal/controller/locationController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class locationController {
-
+	
 	@RequestMapping("")
 	public String index() {
 		System.out.println("[TRACE] index");
@@ -19,5 +19,12 @@ public class locationController {
 		System.out.println("[TRACE] servletHandler");
 		
 		return "servletHandler";
+	}
+	
+	@RequestMapping("postCode.do")
+	public String postCode() {
+		System.out.println("[TRACE] postCode");
+		
+		return "postCode";
 	}
 }
